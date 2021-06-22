053b93b1569ad5852e90663c29d7dff0c770d5c0
Katherine Tam <katherinemtam@gmail.com>
2021-06-21 17:27:07 -0700

    post routes test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,4 +7,18 @@ describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('creates a profile via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/profiles').send({
+        email: 'abc@abc.abc',
+        accountId: 'abc123'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      email: 'abc@abc.abc',
+      accountId: 'abc123'
+    });
+  });
 });
