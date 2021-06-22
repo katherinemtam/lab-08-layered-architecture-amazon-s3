5459d5d4508fb2f3ffb10c15fdd1a7fc25756690
Katherine Tam <katherinemtam@gmail.com>
2021-06-21 21:13:47 -0700

    get (id) route test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Profile from '../lib/models/Profile.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -20,5 +21,18 @@ describe('demo routes', () => {
       email: 'abc@abc.abc',
       accountId: 'abc123'
     });
+  });
+
+  test('gets a profile by id via GET', async () => {
+    const profile = await Profile.insert({
+      email: 'abc@abc.abc',
+      accountId: 'abc123'
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/profiles/${profile.id}`)
+      .send(profile);
+
+    expect(res.body).toEqual(profile);
   });
 });
