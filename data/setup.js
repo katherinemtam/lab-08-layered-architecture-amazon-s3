3c8a502cc9a191f675f706508e1023b352b42e25
Katherine Tam <katherinemtam@gmail.com>
2021-06-21 17:26:50 -0700

    change setup for windows

--- data/setup.js
+++ data/setup.js
@@ -1,18 +1,7 @@
 import { promises as fs } from 'fs';
-import path from 'path';
+// import path from 'path';
 
 export default (pool) => {
-  return fs
-    .readFile(
-      path.join(
-        path.dirname(new URL(import.meta.url).pathname),
-        '..',
-        'sql',
-        'setup.sql'
-      ),
-      {
-        encoding: 'utf-8',
-      }
-    )
-    .then((sql) => pool.query(sql));
+  return fs.readFile('./sql/setup.sql', { encoding: 'utf-8' })
+    .then(sql => pool.query(sql));
 };
